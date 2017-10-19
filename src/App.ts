0de02c342b70d3e520121a155542f7f2b239c134
Webstorm IDE <shekohex@gmailcom>
2017-10-19 05:52:24 +0000

    added schedule worker and Procfile

--- src/App.ts
+++ src/App.ts
@@ -1,5 +1,6 @@
 import Logger from './Logger'
 import { MessageHandler } from './Handler'
+import * as schedule from 'node-schedule'
 const log = Logger('App')
 log.debug('Starting Bot ..')
 //start get stream on DM
@@ -8,8 +9,24 @@ async function startBot(){
     await messageHandler.work()
     await messageHandler.snooze(5000)
 }
-startBot().then(() => {
-    console.log('Done..')
-}).catch(err => {
-    log.error(err)
-})
+const APP = {
+    scheduleJob: () => {
+        // This rule is standard cron syntax for once per minute.
+        // See http://stackoverflow.com/a/5398044/1252653
+        let rule = '*/5 * * * *' //every 5 min
+        // Kick off the job
+       schedule.scheduleJob(rule, () => {
+            console.log('Iam Alive.')
+            startBot().then(() => {
+                console.log('Done..')
+            }).catch(err => {
+                log.error(err)
+            })
+        })
+    },
+    init: () => {
+        APP.scheduleJob()
+    }
+}
+
+APP.init()
